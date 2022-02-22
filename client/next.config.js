676f586170c308af590fbcee2cdedb9918bb11e7
Henil Panchal <37398093+henilp105@users.noreply.github.com>
2022-02-22 14:05:55 +0530

    Update next.config.js

--- client/next.config.js
+++ client/next.config.js
@@ -1,12 +1,19 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: false,
-    async rewrites() {
-        return [
-          {
-            source: '/:path*',
-            destination: 'https://polygon-faucet-api.vercel.app/:path*',
-          },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "https://polygon-faucet-api.vercel.app/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
         ]
-      },
+      }
+    ]
+  }
+};
   };
