a14118da71de5b4c7de1fe6dd82c07b7f2b895c6
Henil Panchal <37398093+henilp105@users.noreply.github.com>
2022-02-22 14:00:03 +0530

    Update next.config.js

--- client/next.config.js
+++ client/next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: false,
-}
+    async rewrites() {
+        return [
+          {
+            source: '/:path*',
+            destination: 'https://polygon-faucet-api.vercel.app/:path*',
+          },
+        ]
+      },
+  };
