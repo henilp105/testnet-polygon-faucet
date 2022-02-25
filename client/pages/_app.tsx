82f55fe665bf9f64819e1f2e9b96a9531dc18760
Henil Panchal <37398093+henilp105@users.noreply.github.com>
2022-02-25 19:52:15 +0530

    Update _app.tsx

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -22,7 +22,6 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta name="robots" content="index, follow" />
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <meta name="language" content="English" />
-        <meta name="revisit-after" content="1 days" />
         <meta name="author" content="Henil Panchal" />
         <meta property="og:type" content="website" />
         <meta property="og:title" content="Polygon Matic Community Faucet" />
