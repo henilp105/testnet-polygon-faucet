0fd8086b43f1e637713a666fa1bc776b48004151
Henil Panchal <37398093+henilp105@users.noreply.github.com>
2022-03-05 16:17:41 +0530

    Update PolygonScan.tsx

--- client/components/PolygonScan.tsx
+++ client/components/PolygonScan.tsx
@@ -10,19 +10,7 @@ const PolygonScan = ({
   short?: boolean
 }) => {
   return (
-    <a
-      href={`https://polygonscan.com/${tx ? 'tx' : 'address'}/${tx ? tx : address}`}
-      target="_blank"
-      rel="noopener noreferrer"
-      className="text-purple-600 dark:text-lime-400 w-16 md:w-auto truncate"
-    >
-      {tx
-        ? short
-          ? tx.substring(0, 15) + '...'
-          : tx
-        : short && address
-        ? address.substring(0, 10) + '...'
-        : address}
+    <a>
     </a>
   )
 }
