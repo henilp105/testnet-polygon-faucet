8cba013b847a112eabed539d244590510a5a0564
Henil Panchal <37398093+henilp105@users.noreply.github.com>
2022-03-05 16:33:23 +0530

    Update _app.tsx

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -46,7 +46,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       <ToastContainer hideProgressBar={true} />
       <main
         className={`flex min-h-screen flex-col items-center justify-center bg-white py-2 dark:bg-slate-900 dark:text-white`}
-      >  THIS IS A TEST NET FAUCET.  This test net Faucet is  Based on Mainnet Faucet by @Zaydme and is Ported by Henil Panchal.\n THIS FACUET IS NOT RECORDING THE SENT TRANSCATIONS. THUS, REQUEST THE DEVELOPERS TO REQUEST RESPONSIBLY.
+      >  THIS IS A TEST NET FAUCET.  This test net Faucet is  Based on Mainnet Faucet by @Zaydme and is Ported by Henil Panchal.
         <Component {...pageProps} />
       </main>
 
