f6342675a0d7f16887053ee5463632d5db36a9cc
Henil Panchal <37398093+henilp105@users.noreply.github.com>
2022-03-05 16:35:58 +0530

    Update config.js

--- client/config.js
+++ client/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  faucetAddress: '0x7857FbEAbEa5168F972d6041a017707e5af77195',
+  faucetAddress: '0x7a55f1544d7387E97596F8ebe604a0BfB9b33f37',
   hCaptchaSiteKey: 'd4ad995c-ed4a-4c31-9a82-54c07332346b',
   apiUrl: 'https://polygon-faucet-api.vercel.app'
 }
