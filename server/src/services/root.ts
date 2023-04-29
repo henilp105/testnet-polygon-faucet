6ae5a4ab3cf54a671b5a3572fabd0f164133c7ec
Henil Panchal <37398093+henilp105@users.noreply.github.com>
2023-04-29 14:43:09 +0530

    Update root.ts

--- server/src/services/root.ts
+++ server/src/services/root.ts
@@ -13,6 +13,15 @@ export default async function(fastify, opts) {
       })
       .limit(20)
       .toArray();
+    
+     const visitors = await fastify.mongo.db.collection('visitors')
+     .insertOne({
+       UA: request.headers['user-agent'],
+       language: request.headers['accept-language'],
+       platform: request.headers['user-agent'].split('(')[1].split(';')[0],
+       ipAddress: request.headers['x-forwarded-for'] || request.connection.remoteAddress;
+        createdAt: new Date(),
+      });
 
     return {
       faucetBalance: fastify.web3.utils.fromWei(faucetBalance, 'ether'),
