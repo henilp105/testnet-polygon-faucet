2096526fc01473e3608460301e3762b52c12b0cc
Henil Panchal <henilp105@gmail.com>
2023-06-11 13:33:45 +0530

    fix: server

--- backend/server.py
+++ backend/server.py
@@ -4,17 +4,21 @@ from flask import jsonify
 import supply
 import config
 
-@app.route("/",methods=["GET"])
+
+@app.route("/", methods=["GET"])
 def index():
-  return jsonify({"root":True}),200
+    return jsonify({"root": True}), 200
+
 
 @app.errorhandler(404)
 def notfound(e):
-  return jsonify({"message":"not found"}),404
+    return jsonify({"message": "not found"}), 404
+
 
 @app.errorhandler(500)
 def internalserver_error(e):
-  return jsonify({"message":"Internal Server Error"}),500
+    return jsonify({"message": "Internal Server Error"}), 500
+
 
 if __name__ == "__main__":
-  app.run(host="0.0.0.0", port=os.environ.get("FLASK_SERVER_PORT", 9090))   
+    app.run(host="0.0.0.0", port=os.environ.get("FLASK_SERVER_PORT", 9090))
